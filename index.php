31a962e913d1222d43b7673b22eae4c3596eed45
francescocanini02 <francesco.canini@enaipriminifad.org>
2023-04-04 17:33:28 +0200

    04

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+// require 'vendor/autoload.php';
+
+// use Ramsey\Uuid\Uuid;
+
+// $uuid = Uuid::uuid4();
+
+// echo $uuid;
+
+
+
+// header("Content-Type: application/json");
+
+// $data = [
+//     'status' => true,
+//     'count' => 3,
+//     'value' => null,
+//     'values' => [1, 3, 7, 9],
+//     'title' => "DEMO"
+// ];
+
+// echo json_encode(
+//     $data
+// );
